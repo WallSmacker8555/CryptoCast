638af2227728db962660f93669d2b7c3e959c11b
mzapodeanu25 <zapodeanu@wisc.edu>
2023-09-30 18:11:28 -0500

    global contract var

--- frontend/smart_contract.js
+++ frontend/smart_contract.js
@@ -1,6 +1,9 @@
 const hre = require("hardhat");
+const { ethers } = require("ethers");
 
 async function getData() {
+    const electionFactory = global.contract;
+    
     const numElections = await electionFactory.numElections();
     let elections;
     for (let i = 0; i < numElections; i++) {
