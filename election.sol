ec5003588569ad1c277e47ce3b6f0c6c733f0431
mzapodeanu25 <zapodeanu@wisc.edu>
2023-09-30 11:01:47 -0500

    added error checking

--- election.sol
+++ election.sol
@@ -1,44 +1,53 @@
-pragma solidity 0.5.1;
+pragma solidity >0.5.1;
 
 contract Election {
-    mapping(uint => bool) public voters;
+    mapping(uint => VotingStatus) public voters;
 //    mapping(string => uint) public canditates;
     
-    string name;
-    address owner;
+    string public name = "Default Name";
+    address public admin;
+    Range public range = Range(0, 0);
     
+    enum VotingStatus {
+        notRegistered,
+        registered,
+        alreadyVoted
+    }
+
     struct Range {
         uint256 openTime;
         uint256 closeTime;
     }
     
-    modifier onlyOwner() {
-        require(msg.sender == owner);
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "only adminstrator can call this function");
         _;
     }
    
-    constructor() public {
-        owner = msg.sender
+    constructor() {
+        admin = msg.sender;
     } 
     
-    function register(uint memory _voterHash) public {
-        require(block.timestamp >= openTime);
-        require(block.timestamp <= closeTime);
-        people[_voterhash] = true;
+    function register(uint _voterHash) public {
+        require(block.timestamp >= range.openTime);
+        require(block.timestamp <= range.closeTime);
+        require(voters[_voterHash] == VotingStatus.notRegistered, "Voted can only register once");
+        voters[_voterHash] = VotingStatus.registered;
     }
     
-    function vote(uint memory _voterHash) public {
-        require(block.timestamp >= openTime);
-        require(block.timestamp <= closeTime);
-        require(people[_voterHash]); 
-        people[_voterhash] = false;
+    function vote(uint _voterHash) public {
+        require(block.timestamp >= range.openTime);
+        require(block.timestamp <= range.closeTime);
+        require(voters[_voterHash] != VotingStatus.notRegistered, "voter must be registered"); 
+        require(voters[_voterHash] != VotingStatus.alreadyVoted, "voter can only vote once"); 
+        voters[_voterHash] = VotingStatus.alreadyVoted;
     }
     
-   function setValidRange(uint256 memory _start, uint256 memory _end) public onlyOwner() {
+   function setValidRange(uint256 _start, uint256 _end) public onlyAdmin() {
         range = Range(_start, _end);
     }
     
-    function setName(string memory _name) public onlyOwner {
+    function setName(string memory _name) public onlyAdmin() {
         name = _name;        
     }
 }
