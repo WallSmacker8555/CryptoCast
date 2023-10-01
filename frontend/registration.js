4dc455d291e5ca1f636465138f6f8af4d0d2554f
WallSmacker8555 <goograh2013@gmail.com>
2023-09-30 21:24:08 -0500

    created hash function

--- frontend/registration.js
+++ frontend/registration.js
@@ -1,6 +1,17 @@
 function getRegData() {
+    let hash = 0;
     var info = document.getElementById("reg_info");
+    var id;
     for (let i = 0; i < info.length; i++) {
-        console.log(info[i].value);
+        if (i == 7) {
+            id = info[i].value;
+        }
+        console.log(info[i]);
     }
+    for (let j = 0; j < id.length; j++) {
+        char = id.charCodeAt(j);
+        hash = ((hash << 5) - hash) + char;
+        hash = hash & hash;
+    }
+    console.log(hash)
 }
