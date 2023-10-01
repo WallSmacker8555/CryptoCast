c9e9c82acd5e36d4d2312be4793a4fd74af2b6ae
WallSmacker8555 <goograh2013@gmail.com>
2023-09-30 21:32:53 -0500

    added hash functionality to voter info page

---
+++ frontend/vote.js
@@ -0,0 +1,17 @@
+function getVoterData() {
+    let hash = 0;
+    var info = document.getElementById("vote_info");
+    var id;
+    for (let i = 0; i < info.length; i++) {
+        if (i == 7) {
+            id = info[i].value;
+        }
+        console.log(info[i]);
+    }
+    for (let j = 0; j < id.length; j++) {
+        char = id.charCodeAt(j);
+        hash = ((hash << 5) - hash) + char;
+        hash = hash & hash;
+    }
+    console.log(hash)
+}
